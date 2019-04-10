0d5c90513b11bb92ae1f45165b8864dc0a484a36
suyini <42664105+suyini@users.noreply.github.com>
2019-04-10 19:54:27 +0800

    Add files via upload

---
+++ Controllers/GCourseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TrainingPlan.Models;
+using TrainingPlan.Web.Api.Repository;
+
+namespace TrainingPlan.Web.Api.Controllers
+{
+    public class GCourseController:ApiController
+    {
+        /*GCourseRepository gcourse = new GCourseRepository();
+
+        [HttpGet]
+        [Route("api/Course/{id}")]
+        public IEnumerable<GeneralCourse> GetByDepartment(string name)
+        {
+            return gcourse.GetByDepartment(name);
+        }*/
+    }
+}
