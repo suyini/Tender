14467cce02350ac1dba0753de745086033bbf311
suyini <42664105+suyini@users.noreply.github.com>
2019-04-11 18:37:33 +0800

    Add files via upload

--- Repository/TeacherRepository.cs
+++ Repository/TeacherRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using TrainingPlan.Data;
 using TrainingPlan.Models;
 using TrainingPlan.Web.Api.Repository;
@@ -11,12 +12,100 @@ namespace TrainingPlan.Web.Api.Repository
     {
         TraingingPlanContext db_context = new TraingingPlanContext();
 
-        //老师根据自己id查询自己编辑的课程
-        /*public IQueryable GetCourse(int id)
+        /// <summary>
+        /// 老师根据自己的ID查看自己编辑的课程
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+       public IQueryable<Course> GetById(int id)
         {
-            return db_context.Courses.Find(id);
-        }*/ 
-        
+
+            var teach = db_context.Teachers
+                        .Single(a => a.TeacherId == id);
+            var postCount = db_context.Entry(teach)
+                         .Collection(b => b.Courses)
+                         .Query();
+            return postCount;
+                         
+
+        }
+
+        /// <summary>
+        /// 老师根据课程审核状态查看课程（未处理，已审核，退回)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        public List<Course> GetByStatus(int id,int status)
+        {
+
+            var teach = db_context.Teachers
+                        .Single(a => a.TeacherId == id);
+            var postCount = db_context.Entry(teach)
+                         .Collection(b => b.Courses)
+                         .Query()
+                         .Where(b => b.Status == status)
+                         .ToList();
+            return postCount;
+
+
+        }
+        /// <summary>
+        /// 审核课程的老师查询审核课程
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        public List<Course> GetCheck(int id, int status)
+        {
+            var teacher = db_context.Teachers;
+            
+                            
+            var course = db_context.Courses;
+            var cour = from a in teacher
+                       join b in course
+                       on a.DepartmenName equals b.DepartmentName
+                       where a.TeacherId==id && b.Status==status
+                       select course 
+                       .ToList();                 
+            return cour;
+          }
+
+            /// <summary>
+            /// 老师根据自己ID查看培养方案（未处理，已审核，退回，已应用）
+            /// </summary>
+            /// <param name="id"></param>
+            /// <param name="status"></param>
+            /// <returns></returns>
+            public List<Scheme> GetSchemeBy(int id,int status)
+        {
+
+            var teach = db_context.Teachers
+                        .Single(a => a.TeacherId == id);
+            var scheme = db_context.Entry(teach)
+                         .Collection(b => b.Schemes)
+                         .Query()
+                         .Where(b => b.Status == status)
+                         .ToList();
+            return scheme;
+
+        }
+
+        public List<Scheme> GetSchemeCourseBy(int id, int status)
+        {
+
+            var teach = db_context.Teachers
+                        .Single(a => a.TeacherId == id);
+            var scheme = db_context.Entry(teach)
+                         .Collection(b => b.Schemes)
+                         .Query()
+                         .Where(b => b.Status == status)
+                         .ToList();
+            return scheme;
+
+        }
+
+
         //添加课程
         public void AddCourse()
         {
